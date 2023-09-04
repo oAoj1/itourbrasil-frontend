d8e868a519b58d55d7c8d91422ff03e9dd7de1ac
oAoj1 <joaogbmarques@gmail.com>
2023-09-04 15:36:46 -0300

    tornando menu um componente

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,58 +1,8 @@
 import './Header.css'
-import { useState } from 'react'
+import Menu from './menu'
 import { SiYourtraveldottv, SiConvertio } from 'react-icons/si'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
-import { BsFillHouseDoorFill } from 'react-icons/bs'
-import { GiBrazil } from 'react-icons/gi'
-import { RiReservedFill } from 'react-icons/ri'
-import { FaBook, FaRoute, FaComments } from 'react-icons/fa'
-import { FiHelpCircle } from 'react-icons/fi'
 
 export default function Header(){
-
-    const [isOpen,setIsOpen] = useState<boolean>(false)
-
-    function abrirMenu(){
-        setIsOpen(!isOpen)
-    }
-
-    const menuHeader = [
-        {
-            opcao:'inicio',
-            icon:<BsFillHouseDoorFill/>
-        },
-        {
-            opcao:'descubra o Brasil',
-            icon:<GiBrazil/>
-        },
-        {
-            opcao:'reservas',
-            icon:<RiReservedFill/>
-        },
-        {
-            opcao:'experiências',
-            icon:<FaBook/>
-        },
-        {
-            opcao:'crie seu roteiro',
-            icon:<FaRoute/>
-        },
-        {
-            opcao:'conversão de moeda',
-            icon:<SiConvertio/>
-        },
-        {
-            opcao:'comentarios',
-            icon:<FaComments/>
-        },
-        {
-            opcao:'dicas',
-            icon:<FiHelpCircle/>
-        }
-        /*
-        'dicas' */
-    ]
-
     return(
         <header>
             <div className="logoHeader">
@@ -63,31 +13,7 @@ export default function Header(){
                 <SiYourtraveldottv/>
                 <h2>Brasil</h2>
             </div>
-            <div className='optionHeader'>
-                <div className="logarRegistrarHeader">
-                    <button>Registrar-se</button>
-                    <button>Logar-se</button>
-                </div>
-                <div className='menuContainer'>
-                    <AiOutlineMenu onClick={abrirMenu} className='abrirMenuIcon'/>
-
-                    <ul className={`menuHeader${isOpen ? 'Open' : ''}`}>
-
-                        <div className="iconsContainer">
-                            <AiOutlineClose className='fecharMenuIcon' onClick={abrirMenu}/>
-                        </div>
-
-                        {menuHeader.map(opcoes => (
-                            <li key={opcoes}>
-                                <button>
-                                    <span>{opcoes.icon}</span>
-                                    <h2>{opcoes.opcao}</h2>
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+            <Menu/>
         </header>   
     )
 }
---
+++ src/components/header/menu/index.tsx
@@ -0,0 +1,81 @@
+import './Menu.css'
+import { useState } from 'react'
+import { SiYourtraveldottv, SiConvertio } from 'react-icons/si'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { BsFillHouseDoorFill } from 'react-icons/bs'
+import { GiBrazil } from 'react-icons/gi'
+import { RiReservedFill } from 'react-icons/ri'
+import { FaBook, FaRoute, FaComments } from 'react-icons/fa'
+import { FiHelpCircle } from 'react-icons/fi'
+
+export default function Menu(){
+
+    const [isOpen,setIsOpen] = useState<boolean>(false)
+
+    function abrirMenu(){
+        setIsOpen(!isOpen)
+    }
+
+    const menuHeader = [
+        {
+            opcao:'inicio',
+            icon:<BsFillHouseDoorFill/>
+        },
+        {
+            opcao:'descubra o Brasil',
+            icon:<GiBrazil/>
+        },
+        {
+            opcao:'reservas',
+            icon:<RiReservedFill/>
+        },
+        {
+            opcao:'experiências',
+            icon:<FaBook/>
+        },
+        {
+            opcao:'crie seu roteiro',
+            icon:<FaRoute/>
+        },
+        {
+            opcao:'conversão de moeda',
+            icon:<SiConvertio/>
+        },
+        {
+            opcao:'comentarios',
+            icon:<FaComments/>
+        },
+        {
+            opcao:'dicas',
+            icon:<FiHelpCircle/>
+        }
+    ]
+
+    return(
+        <div className='optionHeader'>
+            <div className="logarRegistrarHeader">
+                <button>Registrar-se</button>
+                <button>Logar-se</button>
+            </div>
+            <div className='menuContainer'>
+                <AiOutlineMenu onClick={abrirMenu} className='abrirMenuIcon'/>
+
+                <ul className={`menuHeader${isOpen ? 'Open' : ''}`}>
+
+                    <div className="iconsContainer">
+                        <AiOutlineClose className='fecharMenuIcon' onClick={abrirMenu}/>
+                    </div>
+
+                    {menuHeader.map(opcoes => (
+                        <li key={opcoes}>
+                            <button>
+                                <span>{opcoes.icon}</span>
+                                <h2>{opcoes.opcao}</h2>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
